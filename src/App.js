055bbc7ba82d61c534814eaee727423400c60681
Thoriq Nur Faizal <thoriiiq@gmail.com>
2021-04-17 09:50:27 +0700

    Adding materi component based

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Avatar from "./components/Avatar/Avatar";
 
 function App() {
+  // sesuatu didalam return disebut jsx
+
   return (
     <div className="App">
       <h1>Component</h1>
+      <Avatar />
     </div>
   );
 }
