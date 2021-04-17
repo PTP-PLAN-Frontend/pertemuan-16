cbafc8275ea2a8b644ffcfdd052580a6c4837450
Thoriq Nur Faizal <thoriiiq@gmail.com>
2021-04-17 10:23:52 +0700

    Adding card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Avatar from "./components/Avatar/Avatar";
+import Card from "./components/Card/Card";
 
 function App() {
   // sesuatu didalam return disebut jsx
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <h1>Component</h1>
       <Avatar />
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div>
+      <h1>PTP Frontend</h1>
+      <p>Course tentang pelajaran frontend</p>
+    </div>
+  );
+}
+
+export default Card;
